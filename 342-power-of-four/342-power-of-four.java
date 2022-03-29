c128309baa74e09c5172464ffce6a3bab91e07f5
Eshita Gupta <eg5287@gmail.com>
2022-03-30 00:41:31 +0530

    Time: 2 ms (54.42%), Space: 41.5 MB (34.05%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        if(n < 1)
+            return false;
+        if(n==1)
+            return true;
+        if(n%4 != 0)
+            return false;
+        
+        return isPowerOfFour(n/4);
+        
+    }
+}
