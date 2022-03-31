fbe3ce589829566059e4a91a5cf8c73a2f01b0e7
Eshita Gupta <eg5287@gmail.com>
2022-04-01 01:53:41 +0530

    Time: 2 ms (31.09%), Space: 42.5 MB (83.97%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+      
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        LinkedList<Integer> temp = new LinkedList();
+        recurse(ans,temp,nums,0);
+        
+        return ans;
+        
+        
+    }
+    
+    public void recurse(List<List<Integer>> ans,LinkedList <Integer> temp,int[] nums,int index)
+    {
+        if(index == nums.length)
+        {
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        temp.add(nums[index]);
+        recurse(ans,temp,nums,index+1);
+        temp.remove(temp.size() - 1);
+        recurse(ans,temp,nums,index+1);
+    }
+}
