2ae79b5f8ce3debedcefffa619133b9227261cd3
Eshita Gupta <eg5287@gmail.com>
2022-04-29 01:06:42 +0530

    Time: 0 ms (100.00%), Space: 42 MB (87.10%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = n - 1;
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+            {
+                j = j-1;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        
+        return false;
+        
+    }
+}
