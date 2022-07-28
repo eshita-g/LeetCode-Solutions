99e6efc0ea483dbb5dea06525751b567feab486c
Eshita Gupta <eg5287@gmail.com>
2022-07-29 01:56:04 +0530

    Time: 2 ms (69.35%), Space: 41.6 MB (38.56%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int addDigits(int num) {
+        
+        int sum = 0;
+        while(num > 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+            
+            if(num == 0 && sum > 9)
+            {
+                num = sum;
+                sum = 0;
+            }
+        }
+        
+        return sum;
+    }
+}
