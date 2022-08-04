399b2a8c7212c3b04b4fe02d350687237faa8c2b
Eshita Gupta <eg5287@gmail.com>
2022-08-05 04:54:32 +0530

    Time: 2 ms (86.69%), Space: 51.9 MB (40.66%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solve(char[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        int dir[][] = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        for(int j=0;j<n;j++)
+        {
+            if(grid[0][j] == 'O')
+            {    
+              dfs(0,j,grid,dir);
+                  
+            }
+            if(grid[m-1][j] == 'O')
+            {
+                dfs(m-1,j,grid,dir);
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            if(grid[i][0] == 'O')
+            {
+                dfs(i,0,grid,dir);
+            }
+            if(grid[i][n-1] == 'O')
+            {
+                dfs(i,n-1,grid,dir);
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j] == 'O')
+                {
+                    grid[i][j] = 'X';
+                }
+                else if(grid[i][j] == 'P')
+                {
+                    grid[i][j] = 'O';
+                }
+            }
+        }
+            
+    }
+    public void dfs(int i,int j,char[][] grid,int[][] dir)
+    {
+        if(i < 0 || i >= grid.length || j<0 || j>=grid[0].length || grid[i][j]  != 'O')
+            return;
+        
+        grid[i][j] = 'P';
+        for(int k=0;k<4;k++)
+        {
+           dfs(i+dir[k][0], j+dir[k][1],grid,dir);
+        }
+    }
+}
