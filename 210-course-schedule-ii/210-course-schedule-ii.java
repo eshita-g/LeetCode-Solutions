201a2c2a21b646832ab6a4e78aa27a8233ebb6a8
Eshita Gupta <eg5287@gmail.com>
2022-08-09 21:12:21 +0530

    Time: 29 ms (12.37%), Space: 47.6 MB (82.67%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findOrder(int n, int[][] prerequisites) {
+        int[] indeg = new int[n];
+        for(int pre[] : prerequisites)
+        {
+            indeg[pre[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(indeg[i] == 0)
+            {
+                q.add(i);
+            }
+        }
+        ArrayList<Integer> top = new ArrayList<>();
+        while(!q.isEmpty())
+        {
+            int x = q.remove();
+            top.add(x);
+            for(int pre[] : prerequisites)
+            {
+                if(pre[1] == x)
+                {
+                    indeg[pre[0]]--;
+                 if(indeg[pre[0]] == 0)
+                  {
+                    q.add(pre[0]);
+                  }
+                }
+                
+            }
+            
+            
+        }
+        if(top.size() != n)
+        {
+            return new int[0];
+        }
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = top.get(i);
+        }
+        return ans;
+        
+    }
+}
