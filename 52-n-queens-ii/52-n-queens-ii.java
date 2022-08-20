f508cdac13e2db0ad7e0bbe25e819ee6f26d1a0c
Eshita Gupta <eg5287@gmail.com>
2022-08-21 03:35:17 +0530

    Time: 1 ms (98.65%), Space: 40.3 MB (86.20%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    int count  = 0;
+    
+    public void solve(int col,char[][] board,int[] leftRow,int[] lowerDiagnol,int[] upperDiagnol,int n)
+    {
+        if(col == n)
+        {
+            count++;
+            return;
+        }
+        for(int row = 0;row<n;row++)
+        {
+            if(leftRow[row] == 0 && lowerDiagnol[row + col] == 0 && upperDiagnol[n-1 + col-row] == 0)          {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagnol[row + col] = 1;
+                upperDiagnol[n-1 + col - row] = 1;
+                solve(col+1,board,leftRow,lowerDiagnol,upperDiagnol,n);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagnol[row + col] = 0;
+                upperDiagnol[n-1 + col - row] = 0;
+
+
+            }
+        }
+        
+    }
+    public List<String> construct(char[][] board)
+    {
+        List<String> res = new ArrayList<>();
+        for(int i=0;i<board.length;i++)
+        {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        
+        return res;
+    }
+    public int totalNQueens(int n) {
+        
+    
+        char[][] board = new char[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                board[i][j] = '.';
+            }
+        }
+        int[] leftRow = new int[n];
+        int[] lowerDiagnol = new int[2*n - 1];
+        int[] upperDiagnol = new int[2*n - 1];
+        solve(0,board,leftRow,lowerDiagnol,upperDiagnol,n);
+        
+        return count;
+    }
+}
