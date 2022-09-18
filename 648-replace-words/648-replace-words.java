d23ed9ab917aab3b412c3101b1d36ea312c7a2ec
Eshita Gupta <eg5287@gmail.com>
2022-09-18 23:43:46 +0530

    Time: 143 ms (39.45%), Space: 75.5 MB (68.21%) - LeetHub

---
+++ 648-replace-words/648-replace-words.java
@@ -0,0 +1,55 @@
+class Trie{
+    
+    Trie[] child = new Trie[26];
+    String word;
+    
+}
+class Solution {
+    
+    public String replaceWords(List<String> dictionary, String sentence) {
+        
+        int n = dictionary.size();
+        Trie root = new Trie();
+        for(int i=0;i<n;i++)
+        {
+            Trie temp = root;
+            String s = dictionary.get(i);
+            for(int j=0;j<s.length();j++)
+            {
+                if(temp.child[s.charAt(j) - 'a'] == null)
+                {
+                    temp.child[s.charAt(j) - 'a'] = new Trie();
+                }
+                temp = temp.child[s.charAt(j) - 'a'];
+            }
+            temp.word = s;
+        }
+        System.out.println(root.child['b'-'a']);
+
+        StringBuilder ans = new StringBuilder();
+        for(String w : sentence.split("\\s+"))
+        {
+            if(ans.length() > 0)
+            {
+                ans.append(" ");
+            }
+            System.out.println(w);
+            Trie temp = root;
+            for(int i=0;i<w.length();i++)
+            {
+                if(temp.child[w.charAt(i) - 'a'] == null || temp.word != null)
+                {
+                 
+                    System.out.println(temp.child[w.charAt(i) - 'a']);
+                    break;
+                }
+            
+                temp = temp.child[w.charAt(i) - 'a'];
+                    
+            }
+            ans.append(temp.word != null ? temp.word : w);
+        }
+        
+        return ans.toString();
+    }
+}
