036effaa179e09123dde7a788108155f905d03b4
Eshita Gupta <eg5287@gmail.com>
2022-09-25 00:22:49 +0530

    Time: 12 ms (22.09%), Space: 48.5 MB (43.47%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,52 +1,76 @@
 class MinStack {
-   long min;
-    Stack<Long> s;
+    
+    Stack<Long> stk;
+    long curMin;
 
     public MinStack() {
-       s=new Stack<>();
-        min = Long.MAX_VALUE;
+        
+          stk = new Stack<>();
+          curMin = Long.MAX_VALUE;
     }
     
     public void push(int val) {
-        if(s.isEmpty()){
-            min=val;
-            s.push(1l*val);
+        
+         if(stk.isEmpty())
+         {
+             stk.push(1l*val);
+             curMin = 1l*val;
+         }
+        else
+        {
+         if(val >= curMin)
+         {
+             stk.push(1l*val);
+         }
+        else
+        {
+            stk.push(2l*val - curMin);
+            curMin = 1l*val;
         }
-        else{
-            if(min<=val){
-                s.push(1l*val);
-            }
-            else{
-                s.push(2l*val - min);
-                min=val;
-            }
-        }
+        }    
     }
     
     public void pop() {
-        if(s.isEmpty()){return;}
-        long popped=s.pop();
-        if(popped<min){
-            min=2l*min-popped;
-        }
         
+        if(stk.isEmpty())
+            return;
+        long x = stk.pop();
+        if(x < curMin)
+        {
+            curMin = 2l*curMin - x;
+        }
     }
     
     public int top() {
-        if(s.isEmpty()){return -1;}
-        long top=s.peek();
-        if(top<min){
-            return (int)min;
+        
+        if(stk.isEmpty())
+            return -1;
+        
+        
+        long x = stk.peek();
+        
+        if(x >= curMin)
+        {
+            return (int)x;
         }
-        else{
-            return (int)top;
-        }
+        
+        return (int)curMin;
         
     }
     
     public int getMin() {
-        if(s.isEmpty())return -1;
-        return (int)min;
         
+        if(stk.isEmpty())
+            return -1;
+        return (int)curMin;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
