ffbbf4b6c45fba48d6bd2bce26d0e48356c6ec74
Eshita Gupta <eg5287@gmail.com>
2022-12-23 00:48:27 +0530

    Time: 9 ms (81.72%), Space: 48.3 MB (82.25%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int getFood(char[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j] == '*')
+                {
+                    q.add(new int[]{i,j});
+                    break;
+                }
+            }
+        }
+        int res = 0;
+        int[][] vis = new int[m][n];
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                int[] p = q.remove();
+                
+                int x = p[0];
+                int y = p[1];
+                //System.out.println(x+""+y);
+                
+                if(grid[x][y] == '#')
+                {
+                    System.out.println("hi");
+                    return res;
+                }
+                if(vis[x][y] == 1)
+                    continue;
+                
+                vis[x][y] = 1;
+                
+                if(x-1 >= 0 && grid[x-1][y] != 'X')
+                {
+                    q.add(new int[]{x-1,y});
+                }
+                if(x+1 < m && grid[x+1][y] != 'X')
+                {
+                    q.add(new int[]{x+1,y});
+                }
+                if(y-1 >=0 && grid[x][y-1] != 'X')
+                {
+                    q.add(new int[]{x,y-1});
+                }
+                if(y+1 < n && grid[x][y+1] != 'X')
+                {
+                    q.add(new int[]{x,y+1});
+                }
+            }
+            res++;
+        }
+        
+        return -1;
+    }
+}
